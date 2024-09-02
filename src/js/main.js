26937b004cdba531256ff465098b12b16775521b
Neiland85 <theia3impact@gmail.com>
2024-09-02 12:25:13 +0200

    main

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,3 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
-import filterGeniusService from './services/filterGeniusService'
-
-/ / 'Plano'
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-// 'Camera'
-const controls = new OrbitControls(camera, renderer.domElement)
-const gui = new dat.GUI()
-
 // 'process'
 const composer = new EffectComposer(renderer)
 composer.addPass(new RenderPass(scene, camera))

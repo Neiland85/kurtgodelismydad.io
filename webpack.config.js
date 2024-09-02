a5b77dbbcd70c986f8a8d31b8c7714c727b8a72c
Neiland85 <theia3impact@gmail.com>
2024-09-02 06:05:48 +0200

    "add new important 3D files"

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,39 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/main.js',   
+  entry: './src/index.js',  // Archivo de entrada principal
   output: {
-    filename: 'bundle.js',      
-    path: path.resolve(__dirname, 'dist'),  
+    filename: 'bundle.js',  // Nombre del archivo de salida
+    path: path.resolve(__dirname, 'dist'),  // Directorio de salida
+    publicPath: '/'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,         
+        test: /\.js$/,  // Transpilar archivos JavaScript usando Babel
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'], 
-          },
-        },
+          loader: 'babel-loader'
+        }
       },
       {
-        test: /\.css$/,          
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,  
-        use: ['file-loader'],
-      },
-    ],
+        test: /\.(png|jpg|gif|glb|woff2|woff|ttf|eot|svg)$/,  // Manejar archivos estáticos
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/'
+            }
+          }
+        ]
+      }
+    ]
   },
-  mode: 'production',           
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),  // Servir archivos estáticos desde 'public'
+    compress: true,  // Habilitar compresión gzip para todo el contenido servido
+    port: 9000       // Puerto en el que se ejecutará el servidor de desarrollo
+  }
 };
+

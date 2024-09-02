5e3bbc12a542820d941d13bc0c6476843353262f
Neiland85 <theia3impact@gmail.com>
2024-09-03 01:27:38 +0200

    "ad settings and depurated functions in main.js"

--- src/js/main.js
+++ src/js/main.js
@@ -18,14 +18,4 @@ repoCube.position.x = 2
 repoCube.position.y = 2
 repoCube.position.z = 2
 // Luz
-const ambientLight = new THREE.AmbientLight(0x404040)
-scene.add(ambientLight)
-// 'directionalLight'
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
-
-directionalLight.position.set(5, 5, 5).normalize()
-scene.add(directionalLight)
-// 'raycaster'
-const raycaster = new THREE.Raycaster()
-const mouse = new THREE.Vector2()
-window.addEventListener('click',  
+event) => 

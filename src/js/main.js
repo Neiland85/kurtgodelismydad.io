1370aed85707c9c00e9b8a6ae9781aded0dc393a
Neiland85 <theia3impact@gmail.com>
2024-09-02 20:49:49 +0200

    "add main and codeSQL files"

--- src/js/main.js
+++ src/js/main.js
@@ -15,9 +15,12 @@ const repoCubeMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff })
 const repoCube = new THREE.Mesh(repoCubeGeometry, repoCubeMaterial)
 scene.add(repoCube)
 repoCube.position.x = 2
+repoCube.position.y = 2
+repoCube.position.z = 2
 // Luz
 const ambientLight = new THREE.AmbientLight(0x404040)
 scene.add(ambientLight)
+// 'directionalLight'
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
 
 directionalLight.position.set(5, 5, 5).normalize()
@@ -25,48 +28,4 @@ scene.add(directionalLight)
 // 'raycaster'
 const raycaster = new THREE.Raycaster()
 const mouse = new THREE.Vector2()
-window.addEventListener('click', (event) => async function (_params) {
-  { cancelAnimationFrame(animate) }
-  animate() = function () {
-    requestAnimationFrame(animate)
-  }
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
-
-  raycaster.setFromCamera(mouse, camera)
-  const intersects = raycaster.intersectObjects(scene.children)
-
-  if (intersects.length > 0) {
-    intersects[0].object.material.color.set(Math.random() * 0xffffff)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-    repoCube.rotation.x += 0.01
-    repoCube.rotation.y += 0.01
-    controls.update()
-    composer.render()
-    renderer.render(scene, camera)
-  }
-  animate()
-
-  // Redimensiones
-  window.addEventListener('resize', () => {
-    const width = window.innerWidth
-    const height = window.innerHeight
-    renderer.setSize(width, height)
-    camera.aspect = width / height
-    camera.updateProjectionMatrix()
-  })
-  // FilterGenius Botón
-  document.getElementById('filtergenius-button').addEventListener('click', function () {
-    filterGeniusService.filterText('Texto que necesitas filtrar')
-      .then(function (response) {
-        console.log(response)
-        alert('FilterGenius ha respondido: ' + response.data.result)
-      })
-      .catch(more.value() => {
-        return console.error('Error:', eraaror)
-      })
-  })
-}
- 
+window.addEventListener('click', (event) => 
